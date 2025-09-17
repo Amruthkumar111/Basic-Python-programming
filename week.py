a41d35ed8568300a738c65a0f5d2c061cf028a7f
Amruthkumar111 <238w1a02a5@vrsec.ac.in>
2025-09-17 17:07:29 +0530

    Create week.py

---
+++ week.py
@@ -0,0 +1,18 @@
+# to check the week of the day
+a=int(input("Enter a day"))
+if a==1:
+  print("SUNDAY")
+elif a==2:
+  print("MONDAY")
+elif a==3:
+  print("TUESDAY")
+elif a==4:
+  print("WEDNESDAY")
+elif a==5:
+  print("THURSDAY")
+elif a==6:
+  print("FRIDAY")
+elif a==7:
+  print("SATURDAY")
+else:
+  print("invalid day")
