65829bc453f4c7f21d2fb1076314b61be62e8a2e
Amruthkumar111 <238w1a02a5@vrsec.ac.in>
2025-09-18 10:32:11 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,9 @@
+#swap
+a=str(input("Enter the value of a: "))
+b=str(input("Enter the value of b: "))
+temp=a
+a=b
+b=temp
+print("After swapping: ")
+print("First number: ",a)
+print("Second number: ",b)
