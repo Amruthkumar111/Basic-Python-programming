7782d31824946314d894ca429c0bc0f2d3082c63
Amruthkumar111 <238w1a02a5@vrsec.ac.in>
2025-09-18 10:31:01 +0530

    Create avg_3.py

---
+++ avg_3.py
@@ -0,0 +1,5 @@
+#average of three numbers
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+c=int(input("Enter the value of c: "))
+print("Average of three numbers is: ",(a+b+c)/3)
