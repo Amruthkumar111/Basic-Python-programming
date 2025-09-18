18ba32984a4f5b366b15557c9cc38855050212a0
Amruthkumar111 <238w1a02a5@vrsec.ac.in>
2025-09-18 10:29:51 +0530

    Create large_of_3.py

---
+++ large_of_3.py
@@ -0,0 +1,6 @@
+#largest of three numbers
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+c=int(input("Enter the value of c: "))
+largest=max(a,b,c)
+print("Largest number is: ",largest)
