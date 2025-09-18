5ccb0ec6377e78620fced2aff9413f8e406b446b
Amruthkumar111 <238w1a02a5@vrsec.ac.in>
2025-09-18 10:29:01 +0530

    Create arthmetic.py

---
+++ arthmetic.py
@@ -0,0 +1,7 @@
+#arthemetic operations
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+print("Addition is: ",a+b)
+print("Subtraction is: ",a-b)
+print("Multiplication is: ",a*b)
+print("Division is: ",a/b)
